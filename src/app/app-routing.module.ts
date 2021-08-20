cd4ae0742a0ed4c89df5951313d6679c7f765e8f
Fernando <ferre.andrei@yahoo.com>
2021-08-20 18:47:13 +0300

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+
+const routes: Routes = [
+  { path:'', component: MainPageComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
